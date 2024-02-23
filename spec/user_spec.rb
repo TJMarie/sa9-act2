b9d409f31f5cfa77100a9c6602b3de6c266637c7
Adrien Jones <tjones69@memphis.edu>
2024-02-22 19:33:36 -0600

    Initialized rspec and Implemented User class with test cases

---
+++ spec/user_spec.rb
@@ -0,0 +1,23 @@
+require "user"
+RSpec.describe User do
+  describe "#log_in" do
+    it "logs the user in" do
+      user = User.new "test"
+      user.log_in
+      expect(user.logged_in).to be true
+    end
+  end
+  describe "#log_out" do
+    it "logs the user out" do
+      user = User.new "test"
+      user.log_out
+      expect(user.logged_in).to be false
+    end
+  end
+  describe "#username" do
+    it "returns the correct username" do
+      user = User.new "test"
+      expect(user.username).to eq("test")
+    end
+  end
+end
